8132a0a31d03617e88f99cfd8848d0980fcf197e
unknown <kacperbebel97@gmail.com>
2021-04-22 16:26:33 -0400

    added employee folder

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditComponent } from './employee/edit/edit.component';
+import { UnlockComponent } from './employee/unlock/unlock.component';
+import { OrdersComponent } from './employee/orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    UnlockComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
