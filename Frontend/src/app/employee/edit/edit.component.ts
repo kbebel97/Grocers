b393633d6e076808a32af437332d65e5212f08cd
Chris <christopherkngo@gmail.com>
2021-04-23 14:10:03 -0700

    Employee edit page HTML draft

--- Frontend/src/app/employee/edit/edit.component.ts
+++ Frontend/src/app/employee/edit/edit.component.ts
@@ -12,4 +12,21 @@ export class EditComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  resetPassword() {
+    // Get current password id:currPass
+    // Get new password id:newPass
+    // Get confirmation id:confNewPass
+
+    // Check employee password in database
+    //  If password is correct
+    //    Confirm that new password matches confirmation password AND new password is not old password
+    //    If confirmed
+    //      Change employee password in database
+    //      Alert user that password has been changed
+    //      alert("Password has been sucessfully changed");
+    //  If password is incorrect
+    //    Alert user that password was incorrect
+    //    alert("Password is incorrect");
+  }
+
 }
