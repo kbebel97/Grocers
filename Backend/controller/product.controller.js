2ba1b0de1e0da92c36d169c92310d897cededc6e
unknown <kacperbebel97@gmail.com>
2021-04-23 17:04:54 -0400

    cloud db

--- Backend/controller/product.controller.js
+++ Backend/controller/product.controller.js
@@ -1,1 +1,17 @@
 let ProductModel = require("../model/product.model.js");
+
+let sample = (req, res, next) => {
+
+    const product = new ProductModel({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    });
+    console.log(product);
+    product.save();
+    res.status(201).json({
+        message: 'Product added successfully' + product
+    });
+}
+
+module.exports={sample};
