6882cd69589b709119ebe9df00ebabb009db6959
nikhilk2193 <nikhilk558@gmail.com>
2021-04-26 23:28:00 -0500

    Backend Changes for view requests

--- Backend/router/admin.router.js
+++ Backend/router/admin.router.js
@@ -9,6 +9,7 @@ router.post("/addProductDetails", AdminController.addProductDetails);
 router.delete("/deleteProductByName/:name",AdminController.deleteProductByName);
 router.put("/updateProductPrice",AdminController.updateProductPrice);
 router.delete("/deleteEmployeeByEmail/:email", AdminController.deleteEmployeeByEmail);
+router.get("/allEmployeeRequests", AdminController.getAllEmployeeRequests);
 
 
 
