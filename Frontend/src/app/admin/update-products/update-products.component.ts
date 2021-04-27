c9988b0ff3931f14683a8d44e3539060b178f451
Sai Sameera Madhuri Vutukuru <sai_sameera_madhuri.vutukuru@students.tamuk.edu>
2021-04-26 23:13:58 -0500

    Channges made for Add, Delete and Update products

--- Frontend/src/app/admin/update-products/update-products.component.ts
+++ Frontend/src/app/admin/update-products/update-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-update-products',
@@ -6,17 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-products.component.css']
 })
 export class UpdateProductsComponent implements OnInit {
+
 name:any;
 price:any;
 quantity:any;
 description:any;
-  constructor() { }
+message:any;
+
+  constructor(public adminService: AdminService) { }
 
   ngOnInit(): void {
   }
    updateProduct(updateRef: any) {
-    console.log(updateRef);
-
+   console.log(updateRef);
+    this.adminService.updateProductPrice(updateRef).subscribe((result:string)=> {
+      this.message=result;
+     })
     this.name = "";
     this.price = "";
     this.quantity = "";
