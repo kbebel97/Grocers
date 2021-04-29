e4700b6ca4e011aca5d01597c891276ec9cb45e2
nikhilk2193 <nikhilk558@gmail.com>
2021-04-28 23:18:15 -0500

    Changes made for user order status and user locking

--- Frontend/src/app/user/user-order-status/user-order-status.component.ts
+++ Frontend/src/app/user/user-order-status/user-order-status.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Order } from 'src/app/models/Order.model';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserOrdersService } from './user-orders.service';
 
 @Component({
   selector: 'app-user-order-status',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserOrderStatusComponent implements OnInit {
 
-  constructor() { }
+  orders: Array<Order> = [];
+  updatedOrders: Array<Order> = [];
+  sessionObj: any;
+  userName: any;
+
+  @ViewChild('tbody', { read: ElementRef, static: false }) tbody: ElementRef;
+
+  loading: boolean = false;
+
+  constructor(private OrdersService: UserOrdersService) { }
 
   ngOnInit(): void {
+    this.sessionObj = JSON.parse(sessionStorage.loginObject);
+    this.userName = this.sessionObj.userName;
+    this.loading = true;
+    this.OrdersService.retrieveAllOrders().subscribe((result) => {
+      console.log(result.orders)
+      result.orders.forEach(data => {
+        console.log(data.userName);
+        if (this.userName == data.userName) {
+          this.orders.push(data);
+        }
+      })
+      this.loading = false;
+    })
   }
-
 }
