6aa497600c1f3ec782e9cb606442b59988dbfa0c
unknown <kacperbebel97@gmail.com>
2021-04-29 09:47:33 -0400

    empty commit

--- Frontend/src/app/user/user-order-status/user-order-status.component.ts
+++ Frontend/src/app/user/user-order-status/user-order-status.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Order } from 'src/app/models/Order.model';
 
 @Component({
   selector: 'app-user-order-status',
   templateUrl: './user-order-status.component.html',
   styleUrls: ['./user-order-status.component.css']
 })
-export class UserOrderStatusComponent implements OnInit {
+export class UserOrderStatusComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
